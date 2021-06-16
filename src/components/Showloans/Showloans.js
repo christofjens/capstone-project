6e71672522f7df595952b81ca4021e4f090f70ed
Christof Jens <christofjens@gmail.com>
2021-06-16 15:48:23 +0200

    Implement first functionality to get data from API

--- src/components/Showloans/Showloans.js
+++ src/components/Showloans/Showloans.js
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components/macro'
+import { BASE_URL } from '../../helper/url'
 
-export default function ShowLoans() {
-  const [currentBalance, setCurrentBalance] = useState()
+export default function Showloans() {
+  const [availableLoans, setAvailableLoans] = useState([])
 
+  useEffect(() => {
+    fetch(BASE_URL + '/my/loans')
+      .then(res => res.json())
+      .then(resBody => setAvailableLoans(resBody.loans))
+      .catch(err => console.error(err))
+  }, [])
+  console.log(availableLoans)
   return (
     <LoansWrapper>
       <h2>ShowLoans</h2>
       <h3>Current Balance:</h3>
       <p>0 Credits</p>
       <h3>Available Loans:</h3>
-      <p>Starter: 200.000 Credits</p>
+      <p>{availableLoans} 200.000 Credits</p>
       <h3>Taken Loans:</h3>
       <p>none</p>
       <p>You don't have enough credits to repay your loan.</p>
