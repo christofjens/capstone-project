0f167abff1a55933493355f5c8f3f243b1fc8aa1
Christof Jens <christofjens@gmail.com>
2021-06-20 01:44:59 +0200

    Rework Servercheck with useEffect

--- src/components/Servercheck/Servercheck.js
+++ src/components/Servercheck/Servercheck.js
@@ -6,19 +6,17 @@ export default function Servercheck() {
   const [serverStatus, setServerStatus] = useState([])
 
   useEffect(() => {
-    axios
-      .get('https://api.spacetraders.io/game/status')
-      .then(res => setServerStatus(res.data))
-      .catch(err => console.log(err))
-  }, [setServerStatus])
-
-  console.log(serverStatus)
+    const fetchServerStatus = async () => {
+      const result = await axios.get('https://api.spacetraders.io/game/status')
+      setServerStatus(result.data.status)
+    }
+    fetchServerStatus()
+  }, ['https://api.spacetraders.io/game/status'])
 
   return (
     <>
       The Server is currently{' '}
-      {serverStatus.status ===
-      'spacetraders is currently online and available to play'
+      {serverStatus === 'spacetraders is currently online and available to play'
         ? 'active'
         : 'not active'}
     </>
