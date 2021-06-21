8144d102d3a1d80101329a1797a2408147f38b5e
Christof Jens <christofjens@gmail.com>
2021-06-21 10:02:26 +0200

    Add Myloans component

--- src/pages/loans/Loans.js
+++ src/pages/loans/Loans.js
@@ -1,16 +1,21 @@
 // import styled from 'styled-components/macro'
 import { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
 import { loadFromLocal } from '../../helper/localStorage'
 import axios from 'axios'
 import Showloans from '../../components/Showloans/Showloans'
+import Myloans from '../../components/Myloans/Myloans'
+import Takeoutloans from '../../components/Takeoutloans/Takeoutloans'
+import PropTypes from 'prop-types'
 
-Loans.propTypes = {
-  token: PropTypes.string,
+Loans.propTypes ={
+  loantype: PropTypes.string,
+  takeOutLoan: PropTypes.func,
+  
 }
 
-export default function Loans() {
+export default function Loans({takeOutLoan}) {
   const [availableLoans, setAvailableLoans] = useState([])
+  const [takenLoans, setTakenLoans] = useState([])
   const { token } = loadFromLocal('token')
 
   useEffect(() => {
@@ -25,15 +30,22 @@ export default function Loans() {
       )
       setAvailableLoans(result.data.loans)
     })()
-  }, [])
+    ;(async () => {
+      const result = await axios.get('https://api.spacetraders.io/my/loans', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      setTakenLoans(result.data.loans)
+    })()
 
   return (
     <>
       <h2>Loans</h2>
       <h3>Available Loans</h3>
-      <section>
-        {availableLoans.map(
-          ({ amount, collateralRequired, rate, termInDays, type }) => (
+      {availableLoans.map(
+        ({ amount, collateralRequired, rate, termInDays, type }) => (
+          <section>
             <Showloans
               amount={amount}
               type={type}
@@ -41,9 +53,22 @@ export default function Loans() {
               termInDays={termInDays}
               rate={rate}
             />
-          )
-        )}
-      </section>
+            <Takeoutloans takeOutLoan={takeOutLoan} type={type} />
+          </section>
+        )
+      )}
+      <h3>Taken Loans</h3>
+      {takenLoans.map(({ type, status, repaymentAmount, due, id }) => (
+        <section>
+          <Myloans
+            type={type}
+            status={status}
+            repaymentAmount={repaymentAmount}
+            due={due}
+            key={id}
+          />
+        </section>
+      ))}
     </>
   )
 }
